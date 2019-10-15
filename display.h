6fa3b49f5b4e483782a9921ff31a9ba894496c2f
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2019-10-15 15:56:10 +0530

    fix: display: don't print colors on windows

--- display.h
+++ display.h
@@ -4,6 +4,16 @@
 #include "scanner.h"
 #include <stdint.h>
 
+#ifdef _WIN32
+#define ANSI_COLOR_RED ""
+#define ANSI_COLOR_GREEN ""
+#define ANSI_COLOR_YELLOW ""
+#define ANSI_COLOR_BLUE ""
+#define ANSI_COLOR_MAGENTA ""
+#define ANSI_COLOR_CYAN ""
+#define ANSI_COLOR_RESET ""
+#define ANSI_FONT_BOLD ""
+#else
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
@@ -12,6 +22,7 @@
 #define ANSI_COLOR_CYAN "\x1b[36m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 #define ANSI_FONT_BOLD "\x1b[1m"
+#endif
 
 void dbg(const char *msg, ...);
 void err(const char *msg, ...);
