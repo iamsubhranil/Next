a57f52aebf7a3915ef34b1be7ecb2b86d1ea1e92
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2020-04-18 16:47:40 +0530

    new: CMakeLists and initial bits of object model 2.0 The object model of Next is going to be revamped. All allocs and frees throughout Next is going to be tracked by the gc, and the all objects are going to have classes which define the object, and can potentially be exposed to the Next runtime.

---
+++ objecttype.h
@@ -0,0 +1,13 @@
+//
+// Created by iamsubhranil on 3/7/20.
+//
+#ifndef OBJTYPE(name, raw)
+#define OBJTYPE(name, raw)
+#endif
+
+OBJTYPE(Class, NextClass)
+OBJTYPE(Object, NextObject)
+OBJTYPE(Function, Function)
+OBJTYPE(Module, Module)
+
+#undef OBJTYPE
