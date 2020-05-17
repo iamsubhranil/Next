a218c7fbb58c880103a71d902ac90d211145ed0a
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2020-05-17 16:16:55 +0530

    fix: functioncompilationctx: insert bytecode slot on declaration

--- objects/functioncompilationctx.cpp
+++ objects/functioncompilationctx.cpp
@@ -51,6 +51,7 @@ int FunctionCompilationContext::create_slot(String *s, int scopeID) {
 		return (*slotmap)[s].slot;
 	}
 	slotmap[0][s] = (VarState){slotCount++, scopeID};
+	bcc->code->insertSlot();
 	return slotCount - 1;
 }
 
