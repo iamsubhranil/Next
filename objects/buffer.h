1ae3bd0eab0867c9fa2b5e2b522bb2539b7a6383
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2020-05-23 18:03:48 +0530

    new: Buffer This is a scope managed buffer, which stays active for the present scope. The buffer is automatically released in the destructor, and can also be manually freed using release. The buffer performs allocations using the gc. Since the buffer is scope managed, creating a new instance of it explicitly prohibited.

---
+++ objects/buffer.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../gc.h"
+#include "array.h"
+#include <cstdint>
+
+// a scope managed buffer
+template <typename T> class Buffer {
+  private:
+	std::size_t s, cap;
+	T *         buf;
+
+  public:
+	Buffer() {
+		cap = 1;
+		s   = 0;
+		buf = (T *)GcObject_malloc(sizeof(T) * cap);
+	}
+
+	void reserve(std::size_t res) {
+		if(res > cap) {
+			size_t ns = Array::powerOf2Ceil(res);
+			buf = (T *)GcObject_realloc(buf, sizeof(T) * cap, sizeof(T) * ns);
+			cap = ns;
+		}
+	}
+
+	void resize(std::size_t res) {
+		reserve(res + 1);
+		s = res;
+	}
+
+	T *data() { return buf; }
+
+	size_t capacity() { return cap; }
+
+	size_t size() { return s; }
+
+	void release() {
+		GcObject_free(buf, sizeof(T) * cap);
+		buf = NULL;
+		cap = 0;
+		s   = 0;
+	}
+
+	void *operator new(size_t s) = delete;
+
+	~Buffer() { release(); }
+};
