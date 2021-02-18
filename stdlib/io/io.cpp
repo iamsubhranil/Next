ebe55f94bace682991901bfec77b3157bab61040
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-02-18 12:09:38 +0530

    updt: io: open: take relative file paths wrt to the source

--- stdlib/io/io.cpp
+++ stdlib/io/io.cpp
@@ -1,19 +1,52 @@
 #include "io.h"
 
+#include "../../filesystem/path.h"
+#include "../../objects/bytecodecompilationctx.h"
+#include "../../objects/fiber.h"
 #include "../../objects/file.h"
+#include "../../objects/function.h"
 #include "../../printer.h"
+
+String *next_io_create_file_path(String *rel, String *currentPath) {
+	filesystem::path p = filesystem::path((char *)currentPath->strb());
+	if(!p.exists()) {
+		p = filesystem::path::getcwd();
+	} else {
+		p = p.make_absolute().parent_path();
+	}
+	p = p / filesystem::path((char *)rel->strb());
+	return String::from(p.str().c_str());
+}
 
 Value next_io_open1(const Value *args, int numargs) {
 	(void)numargs;
 	EXPECT(io, "open(filename)", 1, String);
-	return File::create(args[1].toString(), String::from("r"));
+	Fiber *   f  = ExecutionEngine::getCurrentFiber();
+	Function *fu = f->callFrames[f->callFramePointer - 1].f;
+	if(fu->getType() == Function::Type::BUILTIN) {
+		RERR("Builtin functions cannot call io.open(_)! Use File::create "
+		     "instead!");
+	}
+	// get the file name from the bytecodecontext
+	String2 fname = String::from(fu->code->ctx->ranges_[0].token.fileName);
+	return File::create(next_io_create_file_path(args[1].toString(), fname),
+	                    String::from("r"));
 }
 
 Value next_io_open2(const Value *args, int numargs) {
 	(void)numargs;
 	EXPECT(io, "open(filename, mode)", 1, String);
 	EXPECT(io, "open(filename, mode)", 2, String);
-	return File::create(args[1].toString(), args[2].toString());
+	Fiber *   f  = ExecutionEngine::getCurrentFiber();
+	Function *fu = f->callFrames[f->callFramePointer - 1].f;
+	if(fu->getType() == Function::Type::BUILTIN) {
+		RERR("Builtin functions cannot call io.open(_)! Use File::create "
+		     "instead!");
+	}
+	// get the file name from the bytecodecontext
+	String2 fname = String::from(fu->code->ctx->ranges_[0].token.fileName);
+	return File::create(next_io_create_file_path(args[1].toString(), fname),
+	                    args[2].toString());
 }
 
 void IO::init(BuiltinModule *b) {
