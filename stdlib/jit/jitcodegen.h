80654e95623cdeedd8af827c6c36bf02ebfdb3fb
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2022-09-01 18:45:11 +0530

    updt: jit: binary/assign/if/while/ret codegen

--- stdlib/jit/jitcodegen.h
+++ stdlib/jit/jitcodegen.h
@@ -1,37 +1,104 @@
 #pragma once
 
+#include <llvm-c/Core.h>
 #include <llvm-c/ExecutionEngine.h>
 #include <llvm-c/Types.h>
 
+#include "../../expr.h"
 #include "../../objects/common.h"
+#include "../../objects/map.h"
+#include "../../stmt.h"
+
+#include <map>
 
 struct Array;
 
-struct JITCodegen {
+struct LLVMCodegenBase : ExpressionVisitor<LLVMValueRef>,
+                         StatementVisitor<void> {
+#define EXPRTYPE(x)                                               \
+	virtual LLVMValueRef visit(x##Expression *s) {                \
+		(void)s;                                                  \
+		return defaultPanic("Not yet implemented codegen for " #x \
+		                    "Expression");                        \
+	}
+#include "../../exprtypes.h"
+
+#define STMTTYPE(x)                                                      \
+	virtual void visit(x##Statement *s) {                                \
+		(void)s;                                                         \
+		defaultPanic("Not yet implemented codegen for " #x "Statement"); \
+	}
+#include "../../stmttypes.h"
+
+	static LLVMValueRef defaultPanic(const char *msg);
+};
+
+struct JITCodegen : LLVMCodegenBase {
+
 	LLVMModuleRef          module;
 	LLVMBuilderRef         builder;
 	LLVMContextRef         ctx;
 	LLVMExecutionEngineRef engine;
-	LLVMValueRef           func;
+	LLVMValueRef           wrapperFunc;
+	LLVMValueRef           compiledFunc;
 	LLVMTypeRef            nextType;
+	LLVMBasicBlockRef      currentBlock;
 
-	void addIsNumber();
-	void addToNumber();
-	void addSetNumber();
-	void addIsBoolean();
-	void addToBoolean();
-	void addSetBoolean();
-	void addIsFalsy();
+	void positionBuilderAtEnd(LLVMBasicBlockRef block);
 
-	LLVMValueRef getConstantInt(int val);
+	static LLVMValueRef getConstantInt(uint64_t val);
 
 	void init();
-	void initValueHelpers();
 	void initEngine();
 
-	LLVMValueRef    getArg(int i);
+#define JITBUILTIN(x, y, z)                  \
+	LLVMValueRef define##x(LLVMValueRef fn); \
+	LLVMValueRef call##x(LLVMValueRef arg);
+#include "jit_builtin.h"
+
+	typedef LLVMValueRef (*LLVMBinInst)(LLVMBuilderRef, LLVMValueRef,
+	                                    LLVMValueRef, const char *);
+	typedef LLVMValueRef (*LLVMBinCmpInst)(LLVMBuilderRef, LLVMRealPredicate,
+	                                       LLVMValueRef, LLVMValueRef,
+	                                       const char *);
+	LLVMValueRef generateBinNumeric(LLVMValueRef left, LLVMValueRef right,
+	                                LLVMBinInst inst, bool isCmp = false,
+	                                LLVMRealPredicate pred = LLVMRealOEQ);
+	void         generateBinBoolean(LLVMValueRef left, LLVMValueRef right,
+	                                LLVMBinInst inst);
+
+	LLVMValueRef    getWrapperArg(int i);
 	next_builtin_fn getCompiledFn();
 	void            gen(Array *stmt);
 
 	static next_builtin_fn compile(Array *statements);
+
+	// map
+	std::map<String *, LLVMValueRef> variableMap;
+	// visitor helpers
+	void         registerArgs(Array *args);
+	LLVMValueRef registerVariable(String *name = NULL);
+	LLVMValueRef registerVariable(Token name);
+	bool         hasVariable(Token name);
+	LLVMValueRef getVariable(Token name);
+	LLVMValueRef getOrRegisterVariable(Token name);
+	void         generateReturn(LLVMValueRef val);
+	LLVMValueRef generateBinOp(LLVMValueRef left, LLVMValueRef right,
+	                           Token::Type op);
+
+	// statement visitors
+	virtual void visit(FnStatement *s);
+	virtual void visit(FnBodyStatement *s);
+	virtual void visit(BlockStatement *s);
+	virtual void visit(ExpressionStatement *s);
+	virtual void visit(ReturnStatement *s);
+	virtual void visit(WhileStatement *s);
+	virtual void visit(IfStatement *s);
+
+	// expression visitors
+	virtual LLVMValueRef visit(AssignExpression *e);
+	virtual LLVMValueRef visit(BinaryExpression *e);
+	virtual LLVMValueRef visit(VariableExpression *e);
+	virtual LLVMValueRef visit(LiteralExpression *e);
+	virtual LLVMValueRef visit(GroupingExpression *e);
 };
